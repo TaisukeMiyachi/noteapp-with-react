cdfdffabab39b3c8c2ce91828320c68e61c5a90c
TaisukeMiyachi <taisuke.m.lotus.elise@gmail.com>
2023-11-11 22:18:16 +0900

    sidebarまで

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./Sidebar.css";
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const Sidebar = ({onAddNote, notes}) => {
+    return (
+        <div className="app-sidebar">
+            <div className="app-sidebar-header">
+                <h1>ノート</h1>
+                <button onClick={onAddNote}>追加</button>
+            </div>
+            <div className="app-sidebar-notes">
+                {notes.map((note) => (
+                <div className="app-sudebar-note">
+                    <div className="sidebar-note-title">
+                            <strong>{note.title}</strong>
+                        <button>削除</button>
+                    </div>
+                        <p>{note.content}</p>
+                        <small>{new Date(note.modDate).toLocaleDateString("ja-Ja", {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                        })}
+                        </small>
+                </div>    
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Sidebar
